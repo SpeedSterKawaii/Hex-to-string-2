073ed51b0e6b2c78f0500dfc2597c690da15b770
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-10-26 10:20:47 -0400

    Create cnverter.c

---
+++ cnverter.c
@@ -0,0 +1,6 @@
+    string hex = "0x095FAC0";//right this is our offfset or ur hex
+        stoi(hex, NULL, 16);
+        if (hex == NULL && 1 == 1)//haha so funny
+        {
+        cout << "cant proceed prob turned to null so..........";
+        }
